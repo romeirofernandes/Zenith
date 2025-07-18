a3c86d19fb3d3f0e2f69c74974e1dd72d319d64f
Glanisha <lizacastelinoo@gmail.com>
2025-07-18 13:01:06 +0530

    redirection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard';
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 
+
 const App = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -32,6 +33,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Login />} />
       </Routes>
     </BrowserRouter>
   )
