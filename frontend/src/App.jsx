f70d939df237a2b38b1fc35b009f7c48c87168d7
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-07-18 11:28:59 +0530

    Initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold text-red-500 bg-blue-300 underline">
+        Team Mota LOG
+      </p>
+    </>
+  )
+}
+
+export default App
